6d62c0abda5a795ef788027be3691e9f954c8816
dariusjecu <dariusjecu@yahoo.com>
2023-02-02 18:07:39 +0000

    New

---
+++ src/assets/images.js
@@ -0,0 +1,10 @@
+import Portfolio1 from './TriviaGame.png'
+import Portfolio2 from './Tenzies.png'
+import Portfolio3 from './MemeGenerator.png'
+import Portfolio4 from './CryptoToken.png'
+import Portfolio5 from './Picsome.png'
+import Portfolio6 from './ToDoList.png'
+import Portfolio7 from "./TicTacToe.png"
+import Portfolio8 from "./IMDb-ish.png"
+
+export {Portfolio1, Portfolio2, Portfolio3, Portfolio4, Portfolio5, Portfolio6, Portfolio7, Portfolio8}
--- src/portfolio/project.jsx
+++ src/portfolio/project.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import {AiFillGithub} from "react-icons/ai"
 import {MdEmojiObjects} from "react-icons/md"
+import {MdOutlineFiberNew} from "react-icons/md"
 
 export default function Project(props){
     return (
         <div className='project'>
+            {props.new ? <MdOutlineFiberNew className='new'/> : <></>}
             <div className='img-container'>
                 <img src={props.img} alt="project-image" />
             </div>
-            <h3>{props.title}</h3>
+            <h3 className='title'>{props.title}{props.inProgress ? <h3 className='inProgress'> - IN PROGRESS</h3> : <></>}</h3>
             <p>{props.desc}</p>
+            <div className='tech'>
+                {props.tech.map((Val, i) => {
+                    return <Val key={i} className="element icon trans"/>
+                })}
+            </div>
             <div className='buttons'>
                 <a className='btn trans' href={props.project} target="_blank"><AiFillGithub/>Github</a>
                 {props.demo!="Token" ? <a className='btn trans' href={props.demo} target="_blank"><MdEmojiObjects/>Live Demo</a> : <></>}
